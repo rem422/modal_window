32755dbb10d67772fd12b173bc546c1e9e88866e
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-01-25 12:55:55 +0300

    Modal Window

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+const modalBtn = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+
+
+modalBtn.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+})
+
+
+
+
+
+
+
