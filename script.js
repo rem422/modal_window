8b0ece69588a27c5f4ef2865ca2e8bd8532cfec0
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-01-25 13:33:03 +0300

    Modal Window

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 'use strict';
-const modalBtn = document.querySelectorAll('.show-modal');
+
 const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
 
-modalBtn.addEventListener('click', () => {
-  modal.classList.remove('hidden');
+btnOpenModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 })
-
-
-
 
 
 
